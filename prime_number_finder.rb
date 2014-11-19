b4cdc8e4ef5d7be6e6520d77b833e3bbc63c017b
Brasten Sager <brasten@brasten.me>
2014-11-19 07:44:09 -0800

    Brute force logic working

--- prime_number_finder.rb
+++ prime_number_finder.rb
@@ -4,9 +4,13 @@ class PrimeNumberFinder
   end
   
   def previous_primes( number )
+    # Base cases
     return [] if number <= 2
     return [2] if number == 3
     
-    
+    # Brute force calculation
+    [2] + (3...number).find_all do |subject|
+      (2...subject).none? { |factor| subject % factor == 0 }
+    end    
   end
 end
