98b125405056c688f1d56a481b98d7bca3f4a538
Brasten Sager <brasten@brasten.me>
2014-11-19 14:36:56 -0800

    Add in caching

--- 46-PrimeNumbers/prime_number_finder.rb
+++ 46-PrimeNumbers/prime_number_finder.rb
@@ -1,6 +1,17 @@
+# An implementation of caching that always misses. A poor caching strategy, obviously.
+#
+class NoCache
+  def get(subject, &block)
+    # just do the thing, return the data
+    block[subject]
+  end
+end
+
 class PrimeNumberFinder
-  def initialize
-    # TODO: Cache
+  attr_reader :cache
+  
+  def initialize(cache: NoCache.new)
+    @cache = cache
   end
   
   def previous_primes( number )
@@ -8,11 +19,12 @@ class PrimeNumberFinder
     return [] if number <= 2
     return [2] if number == 3
     
-    # Still brute-force, but recursively brute force! :-)
-    subject = number - 1
-    previous_primes(subject).tap do |primes|
-      primes << subject if (2...subject).none? { |factor| subject % factor == 0 }
-    end    
+    # Still brute-force, but recursively brute force with CACHING! :-)
+    @cache.get(number - 1) do |subject|
+      previous_primes(subject).tap do |primes|
+        primes << subject if (2...subject).none? { |factor| subject % factor == 0 }
+      end
+    end
   end
 end
 
