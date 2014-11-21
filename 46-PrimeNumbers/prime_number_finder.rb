315630b0efe5b27e2271e20676e5776729b04d0a
Brasten Sager <brasten@brasten.me>
2014-11-21 11:07:17 -0800

    Finished with iterators

--- 46-PrimeNumbers/prime_number_finder.rb
+++ 46-PrimeNumbers/prime_number_finder.rb
@@ -16,45 +16,68 @@ class PrimeNumberFinder
     return [2] if number == 3
 
     primes = [2]
+    number_iter = PrimeCandidateIterator.new(start: 3, max: (number-1))
     
-    subject = 3
-    while( subject < number ) #skipping all even numbers
+    while subject = number_iter.next
+      factor_iter = FactorCandidateIterator.new(primes, subject)
       found_factor = false
-      factor_index = 0
       
-      factor_ceiling = Math.sqrt(subject).ceil
-      factor         = primes[factor_index]
-      
-      while( factor && (factor <= factor_ceiling) )
+      while factor = factor_iter.next
         if subject % factor == 0
           found_factor = true
           break
         end
-        
-        factor_index += 1
-        factor = primes[factor_index]
       end
       
       primes << subject unless found_factor
-      
-      subject += 2
     end
     
     primes
   end
   
+  # Easily iterate over numbers that might be primes. This way we can skip numbers that
+  # couldn't possibly be primes (even numbers, 0, 1).
+  #
   class PrimeCandidateIterator
-    def initialize
-      @next = 2
+    def initialize(start:nil, max:nil)
+      @max  = max
+      @next = (start || 2)
     end
     
+    # @return [Number, nil]
+    #
     def next
-      ret_val = @next
+      return nil if (@max && @max < @next)  # bail
       
-      @next = (@next == 2) ? 3 : @next += 2
-      
-      ret_val      
+      ret_val = @next  
+      @next = (@next == 2) ? 3 : @next += 2      
+      ret_val
     end
+  end
+  
+  # Easily iterate over a subset of prime numbers that could be factors
+  # of the provided subject
+  #
+  class FactorCandidateIterator
+    def initialize(primes, subject)
+      @index = 0
+      @primes = primes  # .dup if thread-safety is more important than perf.
+      
+      # only need to check factors up to the square root
+      # of the subject
+      @max_factor = Math.sqrt(subject).ceil
+    end
+    
+    # @return [Number, nil]
+    #
+    def next
+      factor = @primes[@index]
+      return nil if factor.nil? || factor > @max_factor
+      
+      @index += 1
+      factor
+    end
+  end
 end
 
 Formatter = ->(values) {
