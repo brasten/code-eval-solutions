7d878e59149caedb2709544adceedffdf33ab193
Brasten Sager <brasten@brasten.me>
2015-01-02 13:49:31 -0800

    [140] passing solution

--- 140-DataRecovery/data_recovery.rb
+++ 140-DataRecovery/data_recovery.rb
@@ -1,6 +1,34 @@
+# Repo available at: https://github.com/brasten/code-eval-solutions/
+#
 
+# Processor lambda accepts an input line and returns the result.
+#
+# Process overview:
+#
+# * split up input into arrays of words and offsets
+#
+# * iterate words and offsets, placing word in offset position in
+#   a result array
+#
+# * if the offset array is empty (last word), then our special =xor=
+#   variable should now hold the correct position.
+#
 Processor = ->(input) do
+  words, offsets = input.split(";").map(&:split)
   
+  result  = Array.new(words.size)
+  xors    = 0
+
+  words.each_with_index do |word, idx|
+    xors = xors ^ (idx+1)                 # xor sourcery
+    
+    pos = (offsets.shift || xors).to_i    # get next position
+    xors = xors ^ pos                     # more xor sourcery
+    
+    result[pos-1] = word    
+  end
+  
+  result.join(" ")
 end
 
 
