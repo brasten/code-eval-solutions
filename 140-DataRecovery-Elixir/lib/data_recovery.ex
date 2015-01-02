56faabbea242c675c9293a52796cb5e8dbc8d15c
Brasten Sager <brasten@brasten.me>
2015-01-02 13:19:09 -0800

    [140-exr] Passing specs

--- 140-DataRecovery-Elixir/lib/data_recovery.ex
+++ 140-DataRecovery-Elixir/lib/data_recovery.ex
@@ -1,9 +1,45 @@
 defmodule DataRecovery do  
-  def process(sender, file_name) do
-    send sender, {:ok, "BLAAAH."}
+  def process(line) do
+    resp = line
+      |> parse_line_into_hash
+      |> build_response
+      
+    {:ok, resp}
   end
+  
+  defp build_response(dict) do
+    (1..Dict.size(dict))
+      |> Enum.map(fn(idx) -> dict[idx] || dict[-1] end)
+      |> Enum.join(" ")
+  end
+    
+  defp parse_line_into_hash(line) do
+    line 
+      |> split_into_words_and_indices
+      |> List.zip
+      |> List.foldl( HashDict.new, &fold_element/2 )
+  end
+  
+  defp split_into_words_and_indices(line) do
+    [words, indices] = 
+      String.split( line, [";"] )
+        |> Enum.map( fn(x) -> String.split(x, [" "]) end )
+        
+    [words, indices ++ ["-1"]]
+  end
+  
+  defp fold_element({word, pos}, acc), do: Dict.put(acc, String.to_integer(pos), word)  
 end
 
 if Mix.env() != :test do
   input = System.argv() |> Enum.at(0)
+  
+  if input && File.exists?(input) do
+    File.stream!(input, [:read, :utf8])
+      |> Enum.map( fn(line) -> 
+        String.rstrip(line) 
+          |> DataRecovery.process 
+        end )
+      |> Enum.each( fn({:ok, result}) -> IO.puts(result) end )
+  end
 end
--- 140-DataRecovery-Elixir/test/data_recovery_test.exs
+++ 140-DataRecovery-Elixir/test/data_recovery_test.exs
@@ -2,32 +2,20 @@ defmodule DataRecoveryTest do
   use ExUnit.Case
 
   test "returns correct response for \"2000 and was not However, implemented 1998 it until;9 8 3 4 1 5 7 2\"" do
-    spawn_link(DataRecovery, :process, [self(), "2000 and was not However, implemented 1998 it until;9 8 3 4 1 5 7 2"])
+    {:ok, result} = DataRecovery.process("2000 and was not However, implemented 1998 it until;9 8 3 4 1 5 7 2")
     
-    receive do
-      {:ok, result} -> assert result == "However, it was not implemented until 1998 and 2000"
-    after
-      5_000 -> flunk("No result after 5 seconds!")  
-    end  
+    assert result == "However, it was not implemented until 1998 and 2000"
   end
     
   test "returns correct response for \"programming first The language;3 2 1\"" do
-    spawn_link(DataRecovery, :process, [self(), "programming first The language;3 2 1"])
+    {:ok, result} = DataRecovery.process("programming first The language;3 2 1")
     
-    receive do
-      {:ok, result} -> assert result == "The first programming language"
-    after
-      5_000 -> flunk("No result after 5 seconds!")  
-    end  
+    assert result == "The first programming language"
   end
 
   test "returns correct response for \"programs Manchester The written ran Mark 1952 1 in Autocode from;6 2 1 7 5 3 11 4 8 9\"" do
-    spawn_link(DataRecovery, :process, [self(), "programs Manchester The written ran Mark 1952 1 in Autocode from;6 2 1 7 5 3 11 4 8 9"])
-    
-    receive do
-      {:ok, result} -> assert result == "The Manchester Mark 1 ran programs written in Autocode from 1952"
-    after
-      5_000 -> flunk("No result after 5 seconds!")  
-    end  
+    {:ok, result} = DataRecovery.process("programs Manchester The written ran Mark 1952 1 in Autocode from;6 2 1 7 5 3 11 4 8 9")
+
+    assert result == "The Manchester Mark 1 ran programs written in Autocode from 1952"
   end
 end
