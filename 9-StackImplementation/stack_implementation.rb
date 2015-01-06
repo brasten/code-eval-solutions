097622ddeb28cd1f25756f0c9c6487b2a41bb7d8
Brasten Sager <brasten@brasten.me>
2015-01-06 08:43:38 -0800

    [9] working implementation

--- 9-StackImplementation/stack_implementation.rb
+++ 9-StackImplementation/stack_implementation.rb
@@ -1,10 +1,60 @@
 # Repo available at: https://github.com/brasten/code-eval-solutions/
 #
 
-Processor = ->(input) do
-
+# Stack implementation
+#
+# This is, of course, a silly thing to implement in Ruby. I can't imagine code
+# line this ever ending up in a Ruby application, but it was fun and quick.
+#
+# This is not a robust implementation, either.
+#
+class SagerStack  
+  def initialize()
+    @head = nil
+  end
+  
+  # Pushes an element onto the stack
+  #
+  # @param [Object]
+  #
+  def push(obj)
+    @head = StackElement.new(obj, head)
+  end
+  
+  # Pops an element off of the stack
+  #
+  # @return [Object]
+  #
+  def pop
+    return nil if head.nil?
+    
+    old_head = head
+    @head = head.tail
+    
+    old_head.value
+  end
+  
+  
+  private
+    attr_reader :head
+    
+    StackElement = Struct.new(:value, :tail)
+    
 end
 
+Processor = ->(input) do
+  stack = SagerStack.new
+  
+  input.split.each { |i| stack.push(i.to_i) }
+  
+  arr = []
+  while (elem = stack.pop)
+    stack.pop     # Pop off a second element and drop.
+    arr << elem
+  end
+
+  arr.join(" ")
+end
 
 # Only execute the behavior if this file is being executed directly.
 #
--- 9-StackImplementation/stack_implementation_spec.rb
+++ 9-StackImplementation/stack_implementation_spec.rb
@@ -1,1 +1,19 @@
 require 'stack_implementation'
+
+describe SagerStack do
+  specify "objects can be pushed onto stack and popped off" do
+    stack = SagerStack.new
+    
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
+    stack.push(5)
+    
+    expect( stack.pop ).to eql(5)
+    expect( stack.pop ).to eql(4)
+    expect( stack.pop ).to eql(3)
+    expect( stack.pop ).to eql(2)
+    expect( stack.pop ).to eql(1)
+  end
+end
