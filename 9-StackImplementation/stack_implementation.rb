b9f3e48e4e576a18458a9c1173cddf34b9444d18
Brasten Sager <brasten@brasten.me>
2015-01-06 07:56:46 -0800

    [9] initial checkin of stub

---
+++ 9-StackImplementation/stack_implementation.rb
@@ -0,0 +1,17 @@
+# Repo available at: https://github.com/brasten/code-eval-solutions/
+#
+
+Processor = ->(input) do
+
+end
+
+
+# Only execute the behavior if this file is being executed directly.
+#
+# Otherwise, we're probably trying to test.
+#
+if __FILE__==$0
+  open( ARGV.shift ).each_line do |line|
+    puts Processor[line.chomp] unless line.chomp.empty?
+  end
+end
